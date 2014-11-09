94c71766802e04702d4c87dc09d8c6fc4504dcfd
norabbuccino <bresette@grinnell.edu>
2014-11-09 18:05:19 -0400

    showing movie reviews

--- rottenpotatoes/app/models/moviegoer.rb
+++ rottenpotatoes/app/models/moviegoer.rb
@@ -1,4 +1,5 @@
 class Moviegoer < ActiveRecord::Base
+  has_many :reviews
   attr_accessible :uid, :provider, :name # see text for explanation
   def self.create_with_omniauth(auth)
     Moviegoer.create!(
---
+++ rottenpotatoes/features/step_definitions/review_steps.rb
@@ -0,0 +1,17 @@
+Given /the following moviegoers exist/ do |moviegoers_table|
+  moviegoers_table.hashes.each do |moviegoer|
+    Moviegoer.create! moviegoer
+  end
+end
+
+Given /the following reviews exist/ do |reviews_table|
+  reviews_table.hashes.each do |review|
+    review[:moviegoer] = Moviegoer.find_by_name review[:moviegoer]
+    review[:movie]     = Movie.find_by_title review[:movie]
+    Review.create! review
+  end
+end
+
+Then(/^I should see (\w+)'s review with (\d+) (potato(?:|es))$/) do |name, potatoes, plural|
+  step %Q{I should see "#{potatoes} #{plural} from #{name}"}
+end
